ab3752e5f900222be4ecd62f54baee036964f0dd
Biruk <biruk@Biruks-MBP-2.fios-router.home>
2025-02-15 16:58:39 -0500

    spring initialized, database configured and database credential setted up.

---
+++ ERS-Backend/ERS/src/main/java/com/biruk/ERS/ErsApplication.java
@@ -0,0 +1,13 @@
+package com.biruk.ERS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsApplication.class, args);
+	}
+
+}
