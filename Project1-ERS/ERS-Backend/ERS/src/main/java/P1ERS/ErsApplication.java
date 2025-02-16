b5e0d6b02fa6d163608b12a7a38e5a101d3520f2
Biruk <biruk@Biruks-MBP-2.fios-router.home>
2025-02-15 21:23:33 -0500

    componentScan and EntityScan annotation added in springAPP

--- Project1-ERS/ERS-Backend/ERS/src/main/java/P1ERS/ErsApplication.java
+++ Project1-ERS/ERS-Backend/ERS/src/main/java/P1ERS/ErsApplication.java
@@ -2,8 +2,12 @@ package P1ERS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan("com.biruk.models")
+@ComponentScan("com.biruk")
 public class ErsApplication {
 
 	public static void main(String[] args) {
